34aaf872dcd6a153fdbafd6d380e61bbb4ed6104
Maryam-ra <125599719+Maryam-ra@users.noreply.github.com>
2023-02-25 17:32:16 +0100

    updated time and date

--- src/app.js
+++ src/app.js
@@ -1,19 +1,37 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp); 
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+   }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+   }
+  let days = ["sunday", "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
+  
  let temperatureElement= document.querySelector("#temperature");
  let cityElement= document.querySelector("#city");
  let descriptionElement= document.querySelector("#description");
  let humidityElement= document.querySelector("#humidity");
  let windElement= document.querySelector("#wind");
+ let dateElement= document.querySelector("#date");
  temperatureElement.innerHTML= Math.round(response.data.temperature.current);
  cityElement.innerHTML= (response.data.city);
  descriptionElement.innerHTML= (response.data.condition.description);
  humidityElement.innerHTML= (response.data.temperature.humidity);
  windElement.innerHTML= Math.round(response.data.wind.speed); 
+ dateElement.innerHTML= formatDate(response.data.time * 1000); 
 }
 
 let apiKey = "e6a3600aed10ca53fe3f64d06f1b6ot8";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature);
